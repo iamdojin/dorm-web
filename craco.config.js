0c29cf3692be6680e461eb4dc1de2db165fccb01
iamdojin <kimdojin19@ewhain.net>
2022-06-08 10:17:18 +0900

    build

--- craco.config.js
+++ craco.config.js
@@ -8,8 +8,7 @@ module.exports = {
   },
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
-      paths.appBuild = webpackConfig.output.path =
-        path.resolve("docs");
+      paths.appBuild = webpackConfig.output.path = path.resolve("docs");
       return webpackConfig; // Important: return the modified config
     },
   },
