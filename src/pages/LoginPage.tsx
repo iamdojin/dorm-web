bbbc7cd90b1c84db7e985c9cd3fee4bedd57a3e1
iamdojin <kimdojin19@ewhain.net>
2022-06-08 20:44:18 +0900

    build

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { TextField } from "../components/TextField";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
+import { Button } from "../components/Button";
 
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { replace } = useHistory();
+  const { push, replace } = useHistory();
+  const history = useHistory();
 
   const checkUser = () => {
     if (email === "" || password === "") {
@@ -18,13 +20,13 @@ export const LoginPage = () => {
         identifier: email,
         password: password,
       })
-      .then((response) => {
+      .then((response: { data: { jwt: string } }) => {
         console.log("Login!");
         console.log("token", response.data.jwt);
         localStorage.setItem("token", response.data.jwt);
         replace("/");
       })
-      .catch((error) => {
+      .catch((error: { response: { data: { error: { message: any } } } }) => {
         console.log("error:", error.response.data.error.message);
         alert(error.response.data.error.message);
       });
@@ -38,39 +40,53 @@ export const LoginPage = () => {
 
   return (
     <div className="m-4">
-      <div className="text-3xl font-bold mb-10">로그인</div>
-      <div></div>
       <div>
-        <TextField
-          label="이메일"
-          placeholder="이메일을 입력해주세요."
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <TextField
-          label="비밀번호"
-          placeholder="비밀번호를 입력해주세요."
-          type="password"
-          value={password}
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
+        <img
+          src="dorm.png"
+          alt=""
+          className="w-fit h-fit object-contain"
         />
       </div>
 
-      <div className="mt-10">
-        <div
-          className="bg-gray-800 text-white text-center py-4 rounded-md"
-          onClick={() => {
-            checkUser();
-          }}
-        >
-          로그인
+      <div className="bg-emerald-800 pb-20 pt-20">
+        <div>
+          <TextField
+            className="w-4/5 h-16 mx-auto rounded-lg"
+            labelClassname="font-medium text-2xl ml-16 text-white "
+            label="이메일"
+            placeholder="이메일을 입력해주세요."
+            value={email}
+            onChange={(event: { target: { value: any } }) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <TextField
+            className="w-4/5 h-16 mx-auto rounded-lg"
+            labelClassname="font-medium text-2xl mt-4 ml-16 text-white "
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            value={password}
+            onChange={(event: { target: { value: any } }) => {
+              setPassword(event.target.value);
+            }}
+          />
         </div>
-        <div className="text-center py-4 text-gray-800 border border-gray-800">
-          회원가입
+
+        <div className="mt-10">
+          <div className="bg-black text-white text-center py-4 rounded-lg w-2/4 text-2xl mx-auto">
+            <button
+              onClick={() => {
+                checkUser();
+              }}
+            >
+              로그인
+            </button>
+          </div>
+
+          <div className="text-center text-white text-2xl underline mt-10 mx-auto">
+            <button onClick={() => history.push("/signup")}>회원가입</button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -7,7 +7,6 @@ export const SignupPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
   const { replace } = useHistory();
 
   const register = () => {
@@ -22,8 +21,8 @@ export const SignupPage = () => {
         console.log("Well done!");
         console.log("User profile", response.data.user);
         console.log("User token", response.data.jwt);
-        localStorage.setItem('token', response.data.jwt);
-        replace("/");
+        localStorage.setItem("token", response.data.jwt);
+        replace("/login");
       })
       .catch((error) => {
         // Handle error.
@@ -37,52 +36,82 @@ export const SignupPage = () => {
       replace("/");
     }
   }, []);
+
+  function matchPassword() {
+    var pw1 = document.getElementById("pswd1");
+    var pw2 = document.getElementById("pswd2");
+    if (pw1 != pw2) {
+      alert("Passwords did not match");
+    } else {
+      alert("Passwords confirmed successfully");
+    }
+  }
+
   return (
     <div className="m-4">
-      <div className="text-3xl font-bold mb-10">회원가입</div>
+      <div className="text-3xl text-black font-semibold mt-10 ml-10">
+        회원가입
+      </div>
 
-      <div>
+      <div className="mt-10 bg-emerald-800 pb-20 pt-20 space-y-4">
         <TextField
-          label="이메일"
-          placeholder="이메일을 입력해주세요."
+          className="w-4/5 h-16 mx-auto rounded-lg"
+          labelClassname="font-medium text-xl ml-16 text-white "
+          label="E-mail"
+          placeholder="이메일을 입력해 주세요."
           value={email}
           onChange={(event) => {
             setEmail(event.target.value);
           }}
         />
         <TextField
-          label="비밀번호"
-          placeholder="비밀번호를 입력해주세요."
+          className="w-4/5 h-16 mx-auto rounded-lg"
+          labelClassname="font-medium text-xl ml-16 text-white "
+          label="Password"
+          placeholder="비밀번호를 입력해 주세요."
           value={password}
+          name="pswd1"
           onChange={(event) => {
             setPassword(event.target.value);
           }}
         />
         <TextField
-          label="이름"
-          placeholder="이름을 입력해주세요."
+          className="w-4/5 h-16 mx-auto rounded-lg"
+          placeholder="비밀번호를 한 번 더 입력해 주세요."
+          name="pswd2"
+        />
+        <div className="text-white text-lg bg-black text-center rounded ml-80 mr-16 ">
+          <button
+            onClick={() => {
+              matchPassword();
+            }}
+          >
+            {" "}
+            * Confirm Password
+          </button>
+        </div>
+
+        <TextField
+          className="w-4/5 h-16 mx-auto rounded-lg"
+          labelClassname="font-medium text-xl ml-16 text-white "
+          label="Nickname"
+          placeholder="닉네임을 입력해 주세요."
           onChange={(event) => {
             setName(event.target.value);
-          }}
-        />
-        <TextField
-          label="전화번호"
-          placeholder="전화번호를 입력해주세요."
-          value={phone}
-          onChange={(event) => {
-            setPhone(event.target.value);
           }}
         />
       </div>
 
       <div className="mt-10">
-        <div
-          className="bg-gray-800 text-white text-center py-4 rounded-md"
-          onClick={() => {
-            register();
-          }}
-        >
-          회원가입
+        <div className="bg-black text-white text-center text-2xl py-4 rounded-md w-1/2 mx-auto mb-64">
+          <button
+            onClick={() => {
+              register();
+            }}
+          >
+            {" "}
+            회원가입
+          </button>
         </div>
       </div>
     </div>
